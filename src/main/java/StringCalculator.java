f99367b551c7cd1574a1fcca3966c675a0d4a05b
mehdi <faddak.mehdi@gmail.com>
2024-12-20 09:57:47 +0100

    function calcul null and empty exceptions

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -3,7 +3,8 @@ public class StringCalculator {
     
     public static int calcul(String input) {
         
-
+        if(input == null || input.isEmpty()) return 0;
+        
         
 
         String[] numbers = input.split(",");
