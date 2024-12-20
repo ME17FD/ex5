1c4fb7e9c725c9dc6dfb358ebbd69f510cf1dc7e
mehdi <faddak.mehdi@gmail.com>
2024-12-20 09:50:07 +0100

    created initial tests and code structure

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,12 @@
+public class StringCalculator {
+
+    
+    public static int calcul(String input) {
+        
+
+        
+
+        return 0;
+    }
+}
+
---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,42 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void testEmptyString() {
+        assertEquals(0, StringCalculator.calcul(""));
+    }
+
+    @Test
+    public void testSingleNumber() {
+        assertEquals(20, StringCalculator.calcul("20"));
+    }
+
+    @Test
+    public void testTwoNumbers() {
+        assertEquals(8, StringCalculator.calcul("3,5"));
+    }
+
+    @Test
+    public void testMultipleNumbers() {
+        assertEquals(29, StringCalculator.calcul("10,6,6,7"));
+    }
+
+    @Test
+    public void testEndsWithComma() {
+        assertEquals(29, StringCalculator.calcul("10,6,6,7,"));
+    }
+
+    @Test
+    public void testNewlineSeparator() {
+        assertEquals(5, StringCalculator.calcul("\n2,3"));
+        assertEquals(10, StringCalculator.calcul("5,\n2,3"));
+        assertEquals(10, StringCalculator.calcul("5\n2,3"));
+    }
+
+    @Test
+    public void testNullInput() {
+        assertEquals(0, StringCalculator.calcul(null));
+    }
+}
